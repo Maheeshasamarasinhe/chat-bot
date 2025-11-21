8ef5e492f88e82d7c6dca09dfe3c0db58215655b
Maheesha <maheeshamihiran2021@gmail.com>
2025-11-21 22:36:10 +0530

    Refactor code structure for improved readability and maintainability

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name="medical_chatbot",
+version="0.1.0",
+author="Maheesha Mihiran Samarasinhe",
+author_email="maheeshamihiran2021@gmail.com",
+packages=find_packages(),
+install_requires=[]
+)
