177d1770c0675838769864bc494c4646784917ed
Maheesha <maheeshamihiran2021@gmail.com>
2025-11-22 21:09:25 +0530

    Fix formatting of rules in system prompt for clarity and consistency

--- src/prompt.py
+++ src/prompt.py
@@ -5,9 +5,10 @@ system_prompt = (
     "RULES:"
     "1. If the user asks for treatments, you MUST list the specific drug names, dosages, and regimens found in the context."
     "2. Do NOT summarize medical protocols. Be exact."
-    "3. If the answer is in a table in the context, try to present it clearly."
-    "4. If the user asks in Sinhala, answer in Sinhala,use Sinhala language."
-    "5. If you don't know, say you don't know."
+    "3. If the answer as points  in the context, try to present it clearly."
+    "4. If the answer is in a table in the context, try to present it clearly."
+    "5. If the user asks in Sinhala, answer in Sinhala,use Sinhala language."
+    "6. If you don't know, say you don't know."
     
     "\n\n"
     "{context}"
